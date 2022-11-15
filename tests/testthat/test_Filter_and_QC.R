82a424407d2ea1939a0cdc58b5f574a3f6c4ea5b
Rui He <her2@nih.gov>
2022-11-15 12:37:45 -0500

    Update cc.genes calls

--- tests/testthat/test_Filter_and_QC.R
+++ tests/testthat/test_Filter_and_QC.R
@@ -1,7 +1,8 @@
 test_that("Load testing dataset", {
   datadir <- readRDS("./fixtures/filter_qc_test_in.rds")
   
-  filter_qc_out <- Filter_and_QC(datadir)
+  filter_qc_out <- Filter_and_QC(datadir,
+                                 Organism = "Mouse")
                             
   expected.elements = c("filter_qc_test_h5")
   expect_setequal(names(filter_qc_out), expected.elements)
