b3f19d8af1c99f4864ad8ed149621d8ceed1010a
ammichalowski <michaloa@mail.nih.gov>
2022-12-08 09:14:29 -0800

    update MetadataTable & SampleNames

--- tests/testthat/test-Metadata_Table.R
+++ tests/testthat/test-Metadata_Table.R
@@ -1,18 +1,25 @@
+# load data
+seurat.object <-readRDS(test_path("fixtures", "SO_moduleScore.rds"))
 
+
+# run tests
 test_that("reductions are returned", {
   
-  # load data
-  seurat.object <-
-    readRDS(test_path("fixtures", "SO_moduleScore.rds"))
- 
-  # run function
   metadata <-
     MetadataTable(SO = seurat.object,
                   return.cell.embeddings = TRUE)
   
-  # test
   expect_true(sum(grepl("PC_1$|PC_2$", colnames(metadata))) == 2)
   expect_true(sum(grepl("UMAP_1|UMAP_2", colnames(metadata))) == 2)
   expect_true(sum(grepl("tSNE_1|tSNE_2", colnames(metadata))) == 2)
   
+})
+
+test_that("function returns correct class", {
+  
+  metadata <-
+    MetadataTable(SO = seurat.object,
+                  return.cell.embeddings = TRUE)
+  expect_s3_class(metadata, "data.frame")
+
 })
