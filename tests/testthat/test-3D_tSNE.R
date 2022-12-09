a60aa383f95c410c3227af5dbe5631066695b8bb
maggiec <maggie.cam@nih.gov>
2022-12-09 13:06:47 -0500

    small changes in 3D plotter testing

--- tests/testthat/test-3D_tSNE.R
+++ tests/testthat/test-3D_tSNE.R
@@ -1,8 +1,8 @@
 test_that("Produce 3D tsne plot and return tsne coordinates", {
   
-    seurat_object <- readRDS("/rstudio-files/ccbr-data/users/maggie/SCWorkflow/tests/testthat/fixtures/SO_moduleScore.rds")
+    obj <- readRDS(test_path("fixtures", "SO_moduleScore.rds"))
 
-    tsne.res <- tSNE3D(object,
+    tsne.res <- tSNE3D(object = obj,
                        color.variable = "orig_ident",
                        label.variable = "Likely_CellType",
                        fileName = "tsneplot.html",
