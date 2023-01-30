b52dc2959157851259ac655609e644447e248375
maggiec <maggie.cam@nih.gov>
2023-01-30 13:43:04 -0500

    changes to dotplot

--- R/Dotplot_by_Metadata.R
+++ R/Dotplot_by_Metadata.R
@@ -33,13 +33,35 @@ DotplotMet <- function(object,
   
   #Bring in input genes and custom names
   markers <- gsub("[[:space:]]", "", markers)
+  l1 <- length(markers)
+  dups <- markers[duplicated(markers)]
   markers <- markers[!duplicated(markers)]
+  
+  l2 <- length(markers)
+  if(l1 > l2){message(paste0("\n\nThe following duplicate genes removed: ",dups))}
+  missing.genes <- markers[!markers %in% rownames(object)]
+  
+  l3 <- length(missing.genes)
+  missing.genes <- paste(shQuote(missing.genes), collapse=", ")
+  if(l3 == l2){stop("No genes listed are found in dataset.")}
+  if(l3 > 0){message(paste0("\n\n",l3," genes are absent from dataset:", 
+                          missing.genes,
+                          ". Possible reasons are that gene is not official gene symbol",
+                          " or gene is not highly expressed and has been filtered.\n\n "))}
   markers <- markers[markers %in% rownames(object)]
   cell.type <- cell.type[cell.type != ""]
+  cell.type <- cell.type[cell.type %in% dp$data$id]
+  if(length(cell.type) < 1){
+    error("None of the cell types in list match cell types in metadata")
+  }
+  cell.type.missing <- cell.type[!cell.type %in% dp$data$id]
+  if(length(cell.type.missing) < length(cell.type)){
+    message("")
+  }
+  
   dp <- DotPlot(object, assay="SCT", features=markers, 
                 dot.scale=4,
                 cols = c("lightgrey", dot.color))
-  dp
   dp$data$id <- factor(dp$data$id, levels=rev(cell.type))
   dp$data$features.plot <- factor(dp$data$features.plot, levels=markers)
   
@@ -60,6 +82,85 @@ DotplotMet <- function(object,
   result.list <- list("plot" = plot, "data" = cluster_num)
   
   return(result.list)
+  
+  
+  fs <- input_dataset$fileSystem()
+  path <- fs$get_path("seurat_object.rds", 'r')
+  so <- readRDS(path)
+  
+  metadata.df <- so@meta.data
+  colnames(metadata.df) <- gsub("\\.","_",colnames(metadata.df))
+  Idents(so) <- metadata.df[[metadata_category_to_plot]]
+  
+  #Bring in input category:
+  celltype <- category_labels_and_genes_table[[category_labels_to_plot]][!is.na(category_labels_and_genes_table[[category_labels_to_plot]])]
+  celltype <- celltype[celltype != "null"]
+  
+  #Calculate difference in input category elements from metadata column elements
+  a <- length(unique(Idents(so)))
+  b <- length(celltype)
+  c <- a - b
+  d <- b - a
+  
+  if(sum(celltype %in% Idents(so)) == 0){
+    stop("ERROR: The category from the input table you wish to plot should match the metadata column you are plotting.")
+  } else if(c>0){
+    missinglab <- unique(Idents(so))[!unique(Idents(so)) %in% celltype]
+    print(paste0("There are ",c," elements in the metadata table category that are missing from your input table: "))
+    missinglab <- cat(paste(as.character(missinglab),collapse="\n"))
+    cat("\n")
+  } else if(d>0){
+    missinglab2 <- celltype[!celltype %in% unique(Idents(so))]
+    print(paste0("There are ",d," elements in the category from your input table that are missing from your metadata table: "))
+    missinglab2 <- cat(paste(as.character(missinglab2),collapse="\n"))
+    cat("\n")
+  }
+  
+  #Bring in input gene names:
+  markers <- category_labels_and_genes_table[[genes_column]][!is.na(category_labels_and_genes_table[[genes_column]])]
+  markers <- markers[markers != "null"]
+  
+  #Deal with missing genes:
+  dupmarkers <- markers[duplicated(markers)]
+  if(length(dupmarkers) > 0){
+    dupmarkers <- paste(as.character(dupmarkers),collapse="\n")
+    print(paste0("There are duplicated markers:"))
+    dupmarkers <- cat(dupmarkers)
+    cat("\n")
+  }
+  
+  markers <- markers[!duplicated(markers)]
+  missingmarkers <- markers[!markers %in% rownames(so)]
+  e <- length(missingmarkers)
+  
+  if(e>0){
+    print(paste0("There are ",e," markers that are missing from your single cell dataset: "))
+    missingmarkers <- cat(paste(as.character(missingmarkers),collapse="\n"))
+  }
+  
+  #Draw Dotplot using Seurat function 
+  dp <- DotPlot(so, assay="SCT", features=markers, dot.scale=4,cols = c("lightgrey", dot_color))
+  dp$data$id <- factor(dp$data$id, levels=rev(celltype))
+  dp$data$features.plot <- factor(dp$data$features.plot, levels=markers)
+  dp$data <- na.omit(dp$data) #remove na's which show up in plot
+  
+  #Plot Bubbleplot using Seurat dotplot ggplot object:
+  plot <- ggplot(data = dp$data, mapping = aes_string(x = "features.plot", y = "id")) + 
+    geom_point(mapping = aes_string(size = "pct.exp", color = "avg.exp.scaled")) + 
+    scale.func(range = c(0, 4)) + 
+    theme_cowplot() + theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 90)) + labs(y=metadata_category_to_plot)
+  print(plot)
+  
+  # Show numeric data matching image:
+  if(show_percentage_cells_expressing == TRUE){
+    dp$data %>% select(features.plot,pct.exp,id) %>% pivot_wider(names_from = features.plot, values_from = pct.exp) -> dp.pct.tab
+    return(dp.pct.tab)
+  } else {
+    dp$data %>% select(features.plot,avg.exp,id) %>% pivot_wider(names_from = features.plot, values_from = avg.exp) -> dp.exp.tab
+    return(dp.exp.tab)
+  }
+}
+
 }
 
 
--- tests/testthat/test-Dotplot_by_Metadata.R
+++ tests/testthat/test-Dotplot_by_Metadata.R
@@ -1,14 +1,14 @@
 
+obj <- select_crobject("TEC")
+metadata_column <- "Likely_CellType"
+sample_column <- "orig.ident"
+genes_column <- "Gene_Names"
+cell_type_column <- "Cluster_Names"
+
 test_that("dotplot produced and contingency table returned", {
-  library(Seurat)
-  obj <- readRDS(test_path("fixtures", "SO_moduleScore.rds"))
   
-  metadata_column <- "Likely_CellType"
-  sample_column <- "orig.ident"
-  genes_column <- "Gene_Names"
-  cell_type_column <- "Cluster_Names"
-  
-  markers <- c("Adgre1","Apoe","Arg1","Ccr2","Cd163","Cd38","Cd3d","Cd3e","Cd3g","Cd4","Cd40","Cd68","Cd86","Cd8a","Csf1r","Csf3r","Cx3cr1","Cxcl10","Cxcl9","F13a1","Fn1","Foxp3","G0s2","Gzmk","Il1b","Il2ra","Itgam","Lgals3","Ly6c1","Ly6g","Mafb","Mmp12","Mmp13","Mrc1","Nos2","Nr4a1","S100a8","S100a9","Sell_neg","Siglec1","Vcan")
+  markers <- c("Apoe","Arg1","Cd38","Cd3d","Cd3e","Cd3g","Cd4","Cd40","Cd86","Cd8a","Csf1r","Cx3cr1","Cxcl10",
+               "Cxcl9","F13a1","Fn1","G0s2","Il2ra","Itgam","Lgals3","Ly6c1","Mafb","Nos2","Nr4a1","S100a8","S100a9","Vcan")
   celltypes <- c("CD4_T","cDCs","Monocytes","M1","Neutrophils","B_cells","Tregs","pDC","CD8_T","Macrophages")
   
   results.list <- DotplotMet(object = obj,
@@ -16,10 +16,70 @@ test_that("dotplot produced and contingency table returned", {
                              sample.column = sample_column, 
                              cell.type = celltypes,
                              markers = markers, 
-                             dot.color = "blue")
+                             dot.color = "darkred")
     
-  print(results.list)
   expected.elements <- c("plot","data")
   expect_setequal(names(results.list), expected.elements)
   
 })  
+
+test_that("dotplot run with message for duplicate genes", {
+  
+  markers <- c("Apoe","Apoe","Arg1","Arg1","Cd38","Cd3d","Cd3e","Cd3g","Cd4","Cd40","Cd86","Cd8a","Csf1r","Cx3cr1","Cxcl10",
+               "Cxcl9","F13a1","Fn1","G0s2","Il2ra","Itgam","Lgals3","Ly6c1","Mafb","Nos2","Nr4a1","S100a8","S100a9","Vcan")
+  celltypes <- c("CD4_T","cDCs","Monocytes","M1","Neutrophils","B_cells","Tregs","pDC","CD8_T","Macrophages")
+  
+  expect_message(results.list <- DotplotMet(object = obj,
+                             metadata = metadata_column, 
+                             sample.column = sample_column, 
+                             cell.type = celltypes,
+                             markers = markers, 
+                             dot.color = "darkred"), "The following duplicate genes removed: Apoe")
+  
+
+})  
+
+test_that("dotplot run with message for missing genes", {
+  
+  obj <- readRDS(test_path("fixtures", "SO_moduleScore.rds"))
+  metadata_column <- "Likely_CellType"
+  sample_column <- "orig.ident"
+  genes_column <- "Gene_Names"
+  cell_type_column <- "Cluster_Names"
+  markers <- c("Apoe","Arg1","Cd38","Cd3d","Cd3e","Cd3g","Cd4","Cd40","Cd86","Cd8a","Csf1r","Cx3cr1","Cxcl10",
+               "Cxcl9","F13a1","Fn1","G0s2","Il2ra","Itgam","Lgals3","Ly6c1","Mafb","Nos2","Nr4a1","S100a8","S100a9","Vcan",
+               "Adgre1","Ccr2")
+  celltypes <- c("CD4_T","cDCs","Monocytes","M1","Neutrophils","B_cells","Tregs","pDC","CD8_T","Macrophages")
+  
+  expect_message(results.list <- DotplotMet(object = obj,
+                                            metadata = metadata_column, 
+                                            sample.column = sample_column, 
+                                            cell.type = celltypes,
+                                            markers = markers, 
+                                            dot.color = "darkred"), 
+                 "2 genes are absent from dataset:'Adgre1', 'Ccr2'. Possible reasons are that gene is not official gene symbol or gene is not highly expressed and has been filtered.")
+  
+}) 
+
+test_that("dotplot run with message for missing all genes, eg. human genes instead of mouse", {
+  
+  obj <- readRDS(test_path("fixtures", "SO_moduleScore.rds"))
+  metadata_column <- "Likely_CellType"
+  sample_column <- "orig.ident"
+  genes_column <- "Gene_Names"
+  cell_type_column <- "Cluster_Names"
+  markers <- c("APOE","ARG1","CD38","CD3D","CD3E","CD3G")
+  celltypes <- c("CD4_T","cDCs","Monocytes","M1","Neutrophils","B_cells","Tregs","pDC","CD8_T","Macrophages")
+  
+  expect_error(results.list <- DotplotMet(object = obj,
+                                            metadata = metadata_column, 
+                                            sample.column = sample_column, 
+                                            cell.type = celltypes,
+                                            markers = markers, 
+                                            dot.color = "darkred"), 
+                                            "No genes listed are found in dataset.")
+  
+}) 
+
+
+
