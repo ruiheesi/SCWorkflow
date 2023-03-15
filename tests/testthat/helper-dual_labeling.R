8f5da99ae19390d76e604df02e1b42397fdb3cbd
Maggie Cam <maggie.cam@nih.gov>
2023-03-14 22:40:25 -0400

    Formatting changes to helper-dual labeling

--- tests/testthat/helper-dual_labeling.R
+++ tests/testthat/helper-dual_labeling.R
@@ -1,14 +1,16 @@
-#This helper script will return parameters for each test dependent on the data input:
+#This helper script will return parameters for each test dependent on the data 
+#input ("TEC","Chariou","pbmc-single","nsclc-multi","BRCA"):
 getParamDL <- function(data){
 
   if(data == "TEC"){
       object <- selectCRObject("TEC") 
       samples <- c("1_Embryo_13_5","2_Embryo_15","3_Newborn","4_Adult")
       set.seed(15)
-      marker1 <- sample(rownames(object),1, replace = FALSE)
+      var.feat <- VariableFeatures(object@assays$SCT)[1:100]
+      marker.1 <- sample(var.feat,1)
       marker.1.type = "SCT"
       set.seed(1)
-      marker2 <- sample(rownames(object),1, replace = FALSE)
+      marker.2 <- sample(var.feat,1)
       marker.2.type = "SCT"
       data.reduction = "tsne"
       density.heatmap = TRUE
@@ -16,12 +18,12 @@ getParamDL <- function(data){
   } else if (data == "Chariou") {
       object <- selectCRObject("Chariou")
       samples <- c("PBS","CD8dep","ENT","NHSIL12","Combo")
-      varfeat <- VariableFeatures(object@assays$SCT)[1:100]
+      var.feat <- VariableFeatures(object@assays$SCT)[1:100]
       set.seed(254)
-      marker1 <- sample(varfeat,1)
+      marker.1 <- sample(var.feat,1)
       marker.1.type = "SCT"
       set.seed(35)
-      marker2 <- sample(varfeat,1)
+      marker.2 <- sample(var.feat,1)
       marker.2.type = "SCT"
       data.reduction = "tsne"
       density.heatmap = TRUE
@@ -29,12 +31,12 @@ getParamDL <- function(data){
   } else if (data == "pbmc-single") {
       object <- selectCRObject("pbmc-single")
       samples <- c("PBMC_Single")
-      varfeat <- VariableFeatures(object@assays$SCT)[1:100]
+      var.feat <- VariableFeatures(object@assays$SCT)[1:100]
       set.seed(254)
-      marker1 <- sample(varfeat,1)
+      marker.1 <- sample(var.feat,1)
       marker.1.type = "SCT"
       set.seed(35)
-      marker2 <- sample(varfeat,1)
+      marker.2 <- sample(var.feat,1)
       marker.2.type = "SCT"
       data.reduction = "tsne"
       density.heatmap = TRUE
@@ -42,12 +44,12 @@ getParamDL <- function(data){
   } else if (data == "nsclc-multi") {
       object <- selectCRObject("nsclc-multi")
       samples <- c("Donor_1","Donor_2","Donor_3")
-      varfeat <- VariableFeatures(object@assays$SCT)[1:100]
+      var.feat <- VariableFeatures(object@assays$SCT)[1:100]
       set.seed(254)
-      marker1 <- sample(varfeat,1)
+      marker.1 <- sample(var.feat,1)
       marker.1.type = "SCT"
       set.seed(35)
-      marker2 <- sample(varfeat,1)
+      marker.2 <- sample(var.feat,1)
       marker.2.type = "SCT"
       data.reduction = "tsne"
       density.heatmap = TRUE
@@ -55,12 +57,12 @@ getParamDL <- function(data){
   } else if (data == "BRCA") {
       object <- selectCRObject("BRCA")
       samples <- c("CID4471","CID4290A","CID44971","CID4040","CID4513","CID4535")
-      varfeat <- VariableFeatures(object@assays$SCT)
+      var.feat <- VariableFeatures(object@assays$SCT)
       set.seed(254)
-      marker1 <- sample(varfeat,1)
+      marker.1 <- sample(var.feat,1)
       marker.1.type = "SCT"
       set.seed(35)
-      marker2 <- sample(varfeat,1)
+      marker.2 <- sample(var.feat,1)
       marker.2.type = "SCT"
       data.reduction = "tsne"
       density.heatmap = TRUE
@@ -69,9 +71,9 @@ getParamDL <- function(data){
   
   return(list("object" = object,
               "samples" = samples,
-              "marker1" = marker1,
+              "marker.1" = marker.1,
               "marker.1.type" = marker.1.type,
-              "marker2" <- marker2,
+              "marker.2" <- marker.2,
               "marker.2.type" = marker.2.type,
               "data.reduction" = data.reduction,
               "density.heatmap" = density.heatmap,
