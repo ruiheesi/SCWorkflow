6f4bd6da66ec555ec2c94ad8ef70fc3067c87abf
lobanovav <alexei.lobanov@nih.gov>
2023-03-23 10:48:22 -0400

    Restructured according to new requirements

--- tests/testthat/test-Plot_Metadata.R
+++ tests/testthat/test-Plot_Metadata.R
@@ -1,121 +1,71 @@
 test_that("Test Plot Metadata using TEC (Mouse) dataset", {
+  tec.data <- getParamPM("TEC")
+  output <- do.call(plotMetadata,tec.data)
+
+  ggsave("output/TEC_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet1.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet2.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet3.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[4]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet4.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[5]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet5.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[6]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet6.png")
   
-  obj <- readRDS(test_path("fixtures/TEC", "TEC_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("1_Embryo_13_5","2_Embryo_15","3_Newborn","4_Adult")',
-                                  metadata.to.plot = 'c("SCT_snn_res_0_2","SCT_snn_res_0_4","SCT_snn_res_0_6","SCT_snn_res_0_8","SCT_snn_res_1","SCT_snn_res_1_2")',
-                                  reduction.type = "umap",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
-  
-  
-#  ggsave("output/TEC_PlotMetadata_umap.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","TEC_PlotMetadata_umap.pdf")
-  expect_type(PlotMeta.result,"list")
+    
+  expect_type(output,"list")
   expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
+  expect_setequal(names(output), expected.elements)
   
+
 })
-
-
-
-
-
-test_that("Test Plot Metadata using  using TSNE (TEC Mouse dataset)", {
-  
-  obj <- readRDS(test_path("fixtures/TEC", "TEC_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("1_Embryo_13_5","2_Embryo_15","3_Newborn","4_Adult")',
-                                  metadata.to.plot = 'c("SCT_snn_res_0_2","SCT_snn_res_0_4","SCT_snn_res_0_6","SCT_snn_res_0_8","SCT_snn_res_1","SCT_snn_res_1_2")',
-                                  reduction.type = "tsne",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
-
-#  ggsave("output/TEC_PlotMetadata_tsne.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","TEC_PlotMetadata_tsne.pdf")
-  expect_type(PlotMeta.result,"list")
-  expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
-  
-})
-
-
-
-
-
-
-
-
-
-
-
 
 
 test_that("Test Plot Metadata using Chariou (Mouse) dataset", {
   
-  obj <- readRDS(test_path("fixtures/Chariou", "Chariou_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("CD8dep","Combo","ENT","NHSIL12","PBS")',
-                                  metadata.to.plot = 'c("SCT_snn_res_2_4","SCT_snn_res_2_6","SCT_snn_res_2_8")',
-                                  reduction.type = "umap",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
+  chariou.data <- getParamPM("Chariou")
+  output <- do.call(plotMetadata,chariou.data)
+
+  ggsave("output/Chariou_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","Chariou_plotmet1.png")
+  ggsave("output/Chariou_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","Chariou_plotmet2.png")
+  ggsave("output/Chariou_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","Chariou_plotmet3.png")
   
-  
-#  ggsave("output/Chariou_PlotMetadata_umap.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","Chariou_PlotMetadata_umap.pdf")
-  expect_type(PlotMeta.result,"list")
+  expect_type(output,"list")
   expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
+  expect_setequal(names(output), expected.elements)
   
 })
 
 
 
-
-
-
 test_that("Test Plot Metadata using BRCA (Human) dataset", {
+
+  brca.data <- getParamPM("BRCA")
+  output <- do.call(plotMetadata, brca.data)
+
+  ggsave("output/BRCA_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet1.png")
+  ggsave("output/BRCA_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet2.png")
+  ggsave("output/BRCA_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet3.png")
+  ggsave("output/BRCA_plotmet.png",output$plot[[4]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet4.png")
+  ggsave("output/BRCA_plotmet.png",output$plot[[5]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet5.png")
+  ggsave("output/BRCA_plotmet.png",output$plot[[6]], width = 10, height = 10)
+  expect_snapshot_file("output","BRCA_plotmet6.png")
   
-  obj <- readRDS(test_path("fixtures/BRCA", "BRCA_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("CID3586","CID3838","CID3921","CID3941","CID3946","CID3948","CID3963","CID4040","CID4066","CID4067","CID4290A","CID4398","CID44041","CID4461","CID4463","CID4465","CID4471","CID4495","CID44971","CID44991","CID4513","CID4515","CID45171","CID4523","CID4530N","CID4535")',
-                                  metadata.to.plot = 'c("SCT_snn_res_0_2","SCT_snn_res_0_4","SCT_snn_res_0_6","SCT_snn_res_0_8","SCT_snn_res_1","SCT_snn_res_1_2")',
-                                  reduction.type = "tsne",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
-  
-  
-#  ggsave("output/BRCA_PlotMetadata_tsne.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","BRCA_PlotMetadata_tsne.pdf")
-  expect_type(PlotMeta.result,"list")
+  expect_type(output,"list")
   expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
-  
+  expect_setequal(names(output), expected.elements)
+
 })
 
 
@@ -123,52 +73,83 @@ test_that("Test Plot Metadata using BRCA (Human) dataset", {
 
 test_that("Test Plot Metadata using NSCLCmulti (Human) dataset", {
   
-  obj <- readRDS(test_path("fixtures/NSCLC_Multi", "NSCLCmulti_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("Donor_1","Donor_2","Donor_3","Donor_4","Donor_5","Donor_6","Donor_7")',
-                                  metadata.to.plot = 'c("SCT_snn_res_0_2","SCT_snn_res_0_4","SCT_snn_res_0_6","SCT_snn_res_0_8","SCT_snn_res_1","SCT_snn_res_1_2")',
-                                  reduction.type = "tsne",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
+  nsclc.multi.data <- getParamPM("nsclc-multi")
+  output <- do.call(plotMetadata,nsclc.multi.data)
+
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet1.png")
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet2.png")
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet3.png")
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[4]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet4.png")
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[5]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet5.png")
+  ggsave("output/NSCLCmulti_plotmet.png",output$plot[[6]], width = 10, height = 10)
+  expect_snapshot_file("output","NSCLCmulti_plotmet6.png")
   
-  
-#  ggsave("output/NSCLCmulti_PlotMetadata_tsne.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","NSCLCmulti_PlotMetadata_tsne.pdf")
-  expect_type(PlotMeta.result,"list")
+  expect_type(output,"list")
   expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
+  expect_setequal(names(output), expected.elements)
+
+})
+
+
+
+test_that("Test Plot Metadata using PBMCsingle (Human) dataset", {
+
+
+  pbmc.single.data <- getParamPM("pbmc-single")
+  output <- do.call(plotMetadata,pbmc.single.data)
+
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet1.png")
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet2.png")
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet3.png")
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[4]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet4.png")
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[5]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet5.png")
+  ggsave("output/PBMCsingle_plotmet.png",output$plot[[6]], width = 10, height = 10)
+  expect_snapshot_file("output","PBMCsingle_plotmet6.png")
+  
+  expect_type(output,"list")
+  expected.elements = c("object", "plot")
+  expect_setequal(names(output), expected.elements)
+  
   
 })
 
 
 
-test_that("Test Plot Metadata using NSCLCsingle (Human) dataset", {
-  
-  obj <- readRDS(test_path("fixtures/NSCLC_Single", "NSCLCsingle_Combine_and_Renormalize_SO_downsample.rds"))
-  PlotMeta.result <- plotMetadata(object = obj,
-                                  samples.to.include = 'c("NSCLC_Single")',
-                                  metadata.to.plot = 'c("SCT_snn_res_0_2","SCT_snn_res_0_4","SCT_snn_res_0_6","SCT_snn_res_0_8","SCT_snn_res_1","SCT_snn_res_1_2")',
-                                  reduction.type = "tsne",
-                                  use.cite.seq = FALSE,
-                                  number.of.columns.for.final.image <- 0,
-                                  show.labels = FALSE,
-                                  legend.text.size = 1,
-                                  legend.position = "right",
-                                  columns.to.summarize = "c()",
-                                  summarization.cut.off = 5,
-                                  dot.size = 0.01)
-  
-  
-#  ggsave("output/NSCLCsingle_PlotMetadata_tsne.pdf",PlotMeta.result$plot, width = 10, height = 10)
-#  expect_snapshot_file("output","NSCLCsingle_PlotMetadata_tsne.pdf")
-  expect_type(PlotMeta.result,"list")
+
+test_that("Test Plot Metadata using TEC (Mouse) dataset; UMAP", {
+
+
+  tec.data <- getParamPM("TEC")
+  tec.data$reduction.type <- "umap"
+
+  output <- do.call(plotMetadata,tec.data)
+
+  ggsave("output/TEC_plotmet.png",output$plot[[1]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet1.umap.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[2]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet2.umap.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[3]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet3.umap.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[4]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet4.umap.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[5]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet5.umap.png")
+  ggsave("output/TEC_plotmet.png",output$plot[[6]], width = 10, height = 10)
+  expect_snapshot_file("output","TEC_plotmet6.umap.png")
+
+  expect_type(output,"list")
   expected.elements = c("object", "plot")
-  expect_setequal(names(PlotMeta.result), expected.elements) 
+  expect_setequal(names(output), expected.elements)
   
+
 })
