414723d5f8c1d70fca9aa8b837991bc94021b0af
Maggie Cam <maggie.cam@nih.gov>
2023-04-05 11:45:31 -0400

    Changed pheatmap to ComplexHeatmap::pheatmap

--- tests/testthat/test-Heatmap.R
+++ tests/testthat/test-Heatmap.R
@@ -3,12 +3,7 @@ test_that("Produce heatmap and return plot and filtered dataframe: TEC data", {
     output <- do.call(heatmapSC, cr.object)
             
     expect_snapshot_file(
-      ggsave(
-         "output/TEC_heatmap.png",
-          output$plot,
-          width = 10,
-          height = 10
-        ),
+      .drawpng(output$plot),
         "TEC_heatmap.png"
     )
             
@@ -21,49 +16,44 @@ test_that("Heatmap scaled vs unscaled", {
   cr.object <- getParamHM("TEC")
   cr.object$scale.data <- FALSE
   output2 <- do.call(heatmapSC, cr.object)
-  
+
   expect_snapshot_file(
-    ggsave(
-      "output/TEC_heatmap_unscaled.png",
-      output2$plot,
-      width = 10,
-      height = 10
-    ),
+    .drawpng(output2$plot),
     "TEC_heatmap_unscaled.png"
   )
-  
+
   #compare scaled (a) vs. nonscaled data (b) to be different
   a <- rowMeans(as.data.frame.matrix(output$data)[, -1])
   b <- rowMeans(as.data.frame.matrix(output2$data)[, -1])
-  
+
   expect_false(isTRUE(all.equal(a, b)))
-  
+
 })
 
 test_that("Heatmap run with bad gene name", {
   cr.object <- getParamHM("TEC")
-  
+
   #Spike in with bad gene name
   cr.object$transcripts <- c("badgene", cr.object$transcripts)
-  
+
   expect_warning(do.call(heatmapSC, cr.object), "^There are")
-  
+
   #Actual warning:
   #There are 1 gene(s) absent from dataset:'badgene'.
   #Possible reasons are that gene is not official gene symbol
   #or gene is not highly expressed and has been filtered.
-  
+
 })
 
 test_that("Heatmap run with duplicate gene names", {
   cr.object <- getParamHM("TEC")
-  
+
   #Spike in with bad gene name
   cr.object$transcripts <- c("Map7", cr.object$transcripts)
-  
+
   expect_warning(do.call(heatmapSC, cr.object),
                  "The following duplicate genes were removed: Map7")
-  
+
 })
 
 test_that("Heatmap run with error for missing all genes", {
@@ -72,22 +62,18 @@ test_that("Heatmap run with error for missing all genes", {
     c("APOE", "ARG1", "CD38", "CD3D", "CD3E", "CD3G")
   expect_error(do.call(heatmapSC, cr.object),
                "No genes listed are found in dataset.")
-  
+
 })
 
 test_that("Produce heatmap - Chariou data", {
   cr.object <- getParamHM("Chariou")
   output <- do.call(heatmapSC, cr.object)
-  
+
   expect_snapshot_file(
-    ggsave(
-      "output/Chariou_heatmap.png",
-      output$plot,
-      width = 10,
-      height = 10
-    ),
+    .drawpng(output$plot),
     "Chariou_heatmap.png"
   )
+
   expect_type(output, "list")
   expected.elements = c("plot", "data")
   expect_setequal(names(output), expected.elements)
@@ -96,17 +82,12 @@ test_that("Produce heatmap - Chariou data", {
 test_that("Produce heatmap - PBMC single data", {
   cr.object <- getParamHM("pbmc-single")
   output <- do.call(heatmapSC, cr.object)
-  
+
   expect_snapshot_file(
-    ggsave(
-      "output/pbmc-single_heatmap.png",
-      output$plot,
-      width = 10,
-      height = 10
-    ),
+    .drawpng(output$plot),
     "pbmc-single_heatmap.png"
   )
-  
+
   expect_type(output, "list")
   expected.elements = c("plot", "data")
   expect_setequal(names(output), expected.elements)
@@ -116,17 +97,12 @@ test_that("Produce heatmap with protein using NSCLC multi data", {
   cr.object <- getParamHM("nsclc-multi")
   cr.object$legend.font = 3
   output <- do.call(heatmapSC, cr.object)
-  
+
   expect_snapshot_file(
-    ggsave(
-      "output/nsclc-multi_heatmap.png",
-      output$plot,
-      width = 10,
-      height = 10
-    ),
+    .drawpng(output$plot),
     "nsclc-multi_heatmap.png"
   )
-  
+
   expect_type(output, "list")
   expected.elements = c("plot", "data")
   expect_setequal(names(output), expected.elements)
@@ -135,18 +111,13 @@ test_that("Produce heatmap with protein using NSCLC multi data", {
 test_that("Produce heatmap with filtered dataframe - BRCA data", {
   cr.object <- getParamHM("BRCA")
   output <- do.call(heatmapSC, cr.object)
-  
+
   expect_snapshot_file(
-    ggsave(
-      "output/BRCA_heatmap.png",
-      output$plot,
-      width = 10,
-      height = 10
-    ),
+    .drawpng(output$plot),
     "BRCA_heatmap.png"
   )
-  
+
   expect_type(output, "list")
   expected.elements = c("plot", "data")
   expect_setequal(names(output), expected.elements)
-})  
+})
