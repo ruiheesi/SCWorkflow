b38dd32fb9a6ab1a539d4cdc99bd955cd48bc5e2
Rui He <her2@nih.gov>
2023-06-06 20:01:44 -0400

    Trigger action

--- tests/testthat/test_Combine_and_Renormalize.R
+++ tests/testthat/test_Combine_and_Renormalize.R
@@ -5,6 +5,7 @@ test_that("Load testing dataset", {
   Seurat_Object <- readRDS(test_path("fixtures", "SO_moduleScore.rds"))
   
 
+  
   Combine_and_Renormalize_out <- Combine_and_Renormalize(Seurat_Object,
                                                      npcs = 15,
                                                      vars_to_regress = c("percent.mt"),
